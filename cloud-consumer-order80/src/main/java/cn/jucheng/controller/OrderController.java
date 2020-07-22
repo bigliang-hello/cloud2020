b5e47007389a9917d17363ac66217f8f33e4a7a3
解梁 <>
2020-07-22 23:14:34 +0800

    负载均衡-轮询

--- cloud-consumer-order80/src/main/java/cn/jucheng/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/cn/jucheng/controller/OrderController.java
@@ -2,14 +2,20 @@ package cn.jucheng.controller;
 
 import cn.jucheng.entities.CommonResult;
 import cn.jucheng.entities.Payment;
+import cn.jucheng.lb.LoadBalancer;
+import com.sun.xml.internal.xsom.impl.util.Uri;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.net.URI;
+import java.util.List;
 
 @RestController
 @Slf4j
@@ -20,6 +26,12 @@ public class OrderController {
     @Resource
     private RestTemplate restTemplate;
 
+    @Resource
+    private LoadBalancer loadBalancer;
+
+    @Resource
+    private DiscoveryClient discoveryClient;
+
     @GetMapping("/consumer/payment/create")
     public CommonResult<Payment> create(Payment payment){
         return restTemplate.postForObject(PAYMENT_URL + "payment/create", payment, CommonResult.class);
@@ -28,5 +40,17 @@ public class OrderController {
     @GetMapping("/consumer/payment/{id}")
     public CommonResult<Payment> getPayment(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL + "/payment/" + id, CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/lb")
+    public String getPaymentLB(){
+        List<ServiceInstance> instances = discoveryClient.getInstances("CLOUD-PAYMENT-SERVICE");
+        if (instances == null || instances.size() <= 0){
+            return null;
+        }
+
+        ServiceInstance serviceInstance = loadBalancer.instances(instances);
+        URI uri = serviceInstance.getUri();
+        return restTemplate.getForObject(uri + "/payment/lb", String.class);
     }
 }
--- cloud-provider-payment8002/src/main/java/cn/jucheng/controller/PaymentController.java
+++ cloud-provider-payment8002/src/main/java/cn/jucheng/controller/PaymentController.java
@@ -41,4 +41,9 @@ public class PaymentController {
             return new CommonResult(444, "查询失败", null);
         }
     }
+
+    @GetMapping("/payment/lb")
+    public String getPaymentLB(){
+        return serverPort;
+    }
 }
